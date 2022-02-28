74bfeb735614a7e81ed2c50a09bc65a58fff2ad9
Mk13Ghoul <mayankkumarj5@gmail.com>
2022-02-28 16:17:13 +0530

    Updates

---
+++ precache-manifest.e518c38d07e8066463d634aeaa1292e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b2cbb62ae17f25b4f4fa6e01f63278",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "dda4da552bf29d6be3ab",
+    "url": "/Drum-Machine/static/css/main.effe854a.chunk.css"
+  },
+  {
+    "revision": "0b385abd189849baa3f4",
+    "url": "/Drum-Machine/static/js/2.6d122e07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drum-Machine/static/js/2.6d122e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dda4da552bf29d6be3ab",
+    "url": "/Drum-Machine/static/js/main.f48e62f2.chunk.js"
+  },
+  {
+    "revision": "ce2108411cde3e90c046",
+    "url": "/Drum-Machine/static/js/runtime-main.98ca51cd.js"
+  }
+]);
